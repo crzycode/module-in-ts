fc61748257c773ca3d896b1742052a3dd1cf17d8
75tyle <kamblesanghpal2003@gmail.com>
2023-04-26 21:54:07 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var students_1 = require("../namespace in ts/students");
+var teachers_1 = require("../namespace in ts/teachers");
+var s1 = new students_1.default();
+console.log(s1.data);
+var t1 = new teachers_1.default();
+console.log(t1.data);
---
+++ main.ts
@@ -0,0 +1,8 @@
+import sLogin from '../module in ts/students'
+import tLogin from '../module in ts/teachers'
+
+let s1 = new sLogin()
+console.log(s1.data)
+
+let t1 = new tLogin()
+console.log(t1.data)
